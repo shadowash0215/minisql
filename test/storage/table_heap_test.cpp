9ebc7281556e815d6c43276fa8503c0a49ecb692
shad0wash <shadowash215@gmail.com>
2024-06-09 23:33:44 +0800

    docs: add table_heap_test

--- test/storage/table_heap_test.cpp
+++ test/storage/table_heap_test.cpp
@@ -12,6 +12,11 @@
 static string db_file_name = "table_heap_test.db";
 using Fields = std::vector<Field>;
 
+TEST(TableHeapTest, Reset){
+  std::string db_name = "table_heap_test.db";
+  std::ofstream f(db_name, std::ios::trunc);
+}
+
 TEST(TableHeapTest, TableHeapSampleTest) {
   // init testing instance
   remove(db_file_name.c_str());
@@ -60,4 +65,78 @@ TEST(TableHeapTest, TableHeapSampleTest) {
     delete row_kv.second;
   }
   ASSERT_EQ(size, 0);
+}
+
+TEST(TableHeapTest, TableHeapSelfTest) {
+  setbuf(stdout, NULL);
+  // init testing instance
+  DBStorageEngine engine(db_file_name);
+  const int row_nums = 100;
+  // create schema
+  std::vector<Column *> columns = {new Column("id", TypeId::kTypeInt, 0, false, false),
+                                   new Column("name", TypeId::kTypeChar, 64, 1, true, false),
+                                   new Column("account", TypeId::kTypeFloat, 2, true, false)};
+  auto schema = std::make_shared<Schema>(columns);
+  // create rows
+  std::unordered_map<int64_t, Fields *> row_values;
+
+  TableHeap *table_heap = TableHeap::Create(engine.bpm_, schema.get(), nullptr, nullptr, nullptr);
+  for (int i = 0; i < row_nums; i++) {
+    int32_t len = RandomUtils::RandomInt(0, 64);
+    char *characters = new char[len];
+    RandomUtils::RandomString(characters, len);
+    Fields *fields =
+      new Fields{Field(TypeId::kTypeInt, i), Field(TypeId::kTypeChar, const_cast<char *>(characters), len, true),
+                 Field(TypeId::kTypeFloat, RandomUtils::RandomFloat(-999.f, 999.f))};
+    Row row(*fields);
+    table_heap->InsertTuple(row, nullptr);
+    row_values[row.GetRowId().Get()] = fields;
+    delete[] characters;
+  }
+  // iterator test
+  auto it = table_heap->Begin(nullptr);
+  set<int64_t> Set;
+  for (int i = 0; i < row_nums; i++) {
+    ASSERT((Set.count(it->GetRowId().Get()) == 0), "[error] - iterator error!");
+    Set.insert(it->GetRowId().Get());
+    // make sure the iterator is valid
+    it++;
+  }
+
+  ASSERT_EQ(row_nums, row_values.size());
+  for (auto row_kv : row_values) {
+    Row row(RowId(row_kv.first));
+    table_heap->GetTuple(&row, nullptr);
+    ASSERT_EQ(schema.get()->GetColumnCount(), row.GetFields().size());
+    for (size_t j = 0; j < schema.get()->GetColumnCount(); j++) {
+      ASSERT_EQ(CmpBool::kTrue, row.GetField(j)->CompareEquals(row_kv.second->at(j)));
+    }
+    // free spaces
+    delete row_kv.second;
+  }
+  // update test with new fields
+  string characters = "123";
+  Fields *fields = new Fields{Field(TypeId::kTypeInt, 2000),
+                              Field(TypeId::kTypeChar, const_cast<char *>(characters.c_str()), 3, true),
+                              Field(TypeId::kTypeFloat, RandomUtils::RandomFloat(-999.f, 999.f))};
+  Row row(*fields);
+  table_heap->InsertTuple(row, nullptr);
+  // testget
+  Row testGet(row.GetRowId());
+  table_heap->GetTuple(&testGet, nullptr);
+  for (int i = 0; i < 3; i++) {
+    ASSERT_EQ(CmpBool::kTrue, testGet.GetField(i)->CompareEquals(fields->at(i)));
+  }
+
+  characters = "123";
+  Fields *updated_fields = new Fields{Field(TypeId::kTypeInt, 1000),
+                                      Field(TypeId::kTypeChar, const_cast<char *>(characters.c_str()), 3, true),
+                                      Field(TypeId::kTypeFloat, RandomUtils::RandomFloat(-1.f, 1.f))};
+  Row updated_row(*updated_fields);
+  table_heap->UpdateTuple(updated_row, row.GetRowId(), nullptr);
+  Row testUpdated(row.GetRowId());
+  table_heap->GetTuple(&testUpdated, nullptr);
+  for (int i = 0; i < 3; i++) {
+    ASSERT_EQ(CmpBool::kTrue, testUpdated.GetField(i)->CompareEquals(updated_fields->at(i)));
+  }
 }
